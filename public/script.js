1ee6a1fb30730bbd3a1fe9ffc662be0173dbdbe7
vchatt <ashokside@gmail.com>
2025-06-23 19:59:02 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,69 +1,109 @@
 const socket = io();
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
-let localStream, peer;
+let localStream;
+let peer;
+let currentPartner = null;
 
-async function start() {
+async function init() {
   localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   localVideo.srcObject = localStream;
+
   socket.emit('findPartner', { userId: getUserId(), gender: null });
 }
 
 function getUserId() {
   let id = localStorage.getItem('userId');
   if (!id) {
-    id = 'user-' + Math.random().toString(36).substr(2, 9);
+    id = 'user-' + Math.random().toString(36).substring(2, 10);
     localStorage.setItem('userId', id);
   }
   return id;
 }
 
 socket.on('partnerFound', async (partnerId) => {
-  peer = createPeer(partnerId);
-  localStream.getTracks().forEach(track => peer.addTrack(track, localStream));
+  currentPartner = partnerId;
+  createPeerConnection(partnerId, true);
 });
 
 socket.on('signal', async ({ from, signal }) => {
   if (!peer) {
-    peer = createPeer(from, false);
-    localStream.getTracks().forEach(track => peer.addTrack(track, localStream));
+    currentPartner = from;
+    createPeerConnection(from, false);
   }
-  await peer.setRemoteDescription(new RTCSessionDescription(signal));
-  if (signal.type === 'offer') {
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
-    socket.emit('signal', { to: from, signal: peer.localDescription });
+
+  if (signal.type) {
+    await peer.setRemoteDescription(new RTCSessionDescription(signal));
+    if (signal.type === 'offer') {
+      const answer = await peer.createAnswer();
+      await peer.setLocalDescription(answer);
+      socket.emit('signal', { to: from, signal: peer.localDescription });
+    }
+  } else {
+    try {
+      await peer.addIceCandidate(new RTCIceCandidate(signal));
+    } catch (err) {
+      console.warn('Error adding ICE candidate', err);
+    }
   }
 });
 
-function createPeer(partnerId, initiator = true) {
-  const pc = new RTCPeerConnection();
-  pc.onicecandidate = e => {
+socket.on('userDisconnected', (id) => {
+  if (id === currentPartner) {
+    console.log('Partner disconnected');
+    cleanup();
+  }
+});
+
+function createPeerConnection(partnerId, isInitiator) {
+  peer = new RTCPeerConnection({
+    iceServers: [
+      { urls: 'stun:stun.l.google.com:19302' }
+    ]
+  });
+
+  peer.onicecandidate = (e) => {
     if (e.candidate) {
       socket.emit('signal', { to: partnerId, signal: e.candidate });
     }
   };
-  pc.ontrack = e => {
+
+  peer.ontrack = (e) => {
+    console.log('Remote stream added');
     remoteVideo.srcObject = e.streams[0];
   };
-  if (initiator) {
-    pc.onnegotiationneeded = async () => {
-      const offer = await pc.createOffer();
-      await pc.setLocalDescription(offer);
-      socket.emit('signal', { to: partnerId, signal: pc.localDescription });
+
+  localStream.getTracks().forEach(track => peer.addTrack(track, localStream));
+
+  if (isInitiator) {
+    peer.onnegotiationneeded = async () => {
+      try {
+        const offer = await peer.createOffer();
+        await peer.setLocalDescription(offer);
+        socket.emit('signal', { to: partnerId, signal: peer.localDescription });
+      } catch (err) {
+        console.error('Negotiation error:', err);
+      }
     };
   }
-  return pc;
+}
+
+function cleanup() {
+  if (peer) {
+    peer.close();
+    peer = null;
+  }
+  remoteVideo.srcObject = null;
 }
 
 document.getElementById('nextBtn').onclick = () => {
-  if (peer) peer.close();
-  start();
+  cleanup();
+  socket.emit('findPartner', { userId: getUserId(), gender: null });
 };
 
 document.getElementById('disconnectBtn').onclick = () => {
-  if (peer) peer.close();
+  cleanup();
   localVideo.srcObject.getTracks().forEach(track => track.stop());
   remoteVideo.srcObject = null;
 };
-start();
+init();
